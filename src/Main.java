cd37aacc0778fc6e4211c05b45bd095a557ea63a
dorav <doravar@gmail.com>
2022-07-30 09:38:00 +0200

    comment1

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+
     }
 
 }
